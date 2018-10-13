269a8d31fc3bbf4a8dc409b82a113557e91dae97
141naoto <naot.ishii141@gmail.com>
2018-10-13 13:25:44 +0900

    admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,21 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   end
 
+  def after_sign_in_path_for(resource)
+  	case resource
+  	when User
+  		session[:previous_url] || root_path
+  	when Admin
+  		admin_user_path(current_admin.id)
+  	end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
+
 end
